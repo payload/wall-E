9d156139dbc8bb6122bdb8f3aadd0fa972f1ee84
dodo <dodo@blacksec.org>
2011-11-23 03:22:10 +0100

    fireflies

--- blob/main.lua
+++ blob/main.lua
@@ -52,7 +52,7 @@ end
 
 -- values
 
-nr = 13
+nr = 42
 blobs = {}
 
 
@@ -66,25 +66,27 @@ local __maxd = 0
 
 function update()
     for _, blob in ipairs(blobs) do
-        local d = 0.3
+        local d = 0.9
         blob.dir.x = (blob.dir.x + R()*d - d*0.5)*0.9
         blob.dir.y = (blob.dir.y + R()*d - d*0.5)*0.9
 
-        blob.strength = (blob.strength + R()*10 - 5)*0.8
+        --blob.strength = (blob.strength + R()*10 - 5)*0.8
         --blob.radius = (blob.radius     + R()*0.1 - 0.05)
 
         blob.x = blob.x + blob.dir.x
         blob.y = blob.y + blob.dir.y
 
+        local w = wall.width * __maxbound - wall.width
         if blob.x > wall.width * __maxbound then
-            blob.x = - wall.width * __maxbound
-        elseif blob.x < - wall.width * __maxbound then
+            blob.x = - w
+        elseif blob.x < - w then
             blob.x = wall.width * __maxbound
         end
 
+        local h = wall.height * __maxbound - wall.height
         if blob.y > wall.height * __maxbound then
-            blob.y = - wall.height * __maxbound
-        elseif blob.y < - wall.height * __maxbound then
+            blob.y = - h
+        elseif blob.y < - h then
             blob.y = wall.height * __maxbound
         end
 
@@ -92,7 +94,17 @@ function update()
         for k, c in pairs(blob.color) do
             blob.color[k] = c + R()*10 - 5
         end
+
+        if tick%50 == 0 then
+            local c = blob.color
+            local tmp = c.r
+            c.r = c.g
+            c.g = c.b
+            c.b = tmp
+        end
     end
+
+    tick = tick + 1
 end
 
 function draw()
@@ -120,9 +132,16 @@ function draw()
                 b = b + blob.color.b * d
             end
 
-            r = inbound(r * __reznr,0,255)
-            g = inbound(g * __reznr,0,255)
-            b = inbound(b * __reznr,0,255)
+            local m = 1
+            --local sum = (r + g + b)/3
+            --local m = inbound(sum/256)--inbound(math.sqrt(sum))
+            --if m == 0 then m = -m end
+            --if sum < 33 and sum > 188 then m = 0 end
+
+
+            r = inbound(m*r * __reznr,0,255)
+            g = inbound(m*g * __reznr,0,255)
+            b = inbound(m*b * __reznr,0,255)
 
             --print(x,y,r,g,b)
 
@@ -155,8 +174,8 @@ function love.load()
             strength = (R()*9 + 1),
             radius = R()+0.5,
             color = {r=0,g=0,b=0},--{ r=R(150,200), g=R(150,200), b=R(150.200) },
-            x = R()*wall.width*__maxbound - wall.width*__maxbound*0.5,
-            y = R()*wall.height*__maxbound - wall.height*__maxbound*0.5,
+            x = R()*wall.width*__maxbound,
+            y = R()*wall.height*__maxbound,
         }
 
         for i, c in ipairs(shuffle({"r","g","b"})) do
